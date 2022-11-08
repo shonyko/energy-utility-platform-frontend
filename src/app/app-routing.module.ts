019f6c7b664a128010eaae1b122520efc521cb69
Alex Kovacs <alexkovacs@rocketmail.com>
2022-11-08 06:05:29 +0200

    Add authentication

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {NotFoundPageComponent} from "./pages/not-found-page/not-found-page.component";
+import {LoginPageComponent} from "./pages/login-page/login-page.component";
+import {RegisterPageComponent} from "./pages/register-page/register-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login', component: LoginPageComponent
+  },
+  {
+    path: 'register', component: RegisterPageComponent
+  },
+  {
+    path: '**', component: NotFoundPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {FlexLayoutModule} from "@angular/flex-layout";
+import { HttpClientModule } from '@angular/common/http';
+
+import {AngularMaterialModule} from "./angular.material.module";
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundPageComponent,
+    LoginPageComponent,
+    RegisterPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    ReactiveFormsModule,
+    FormsModule,
+    FlexLayoutModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
